253a7eea4230294a37fc4c36747bb2a59db59784
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-05-15 17:22:19 -0300

    Algoritmo KNN

---
+++ KNN.py
@@ -0,0 +1,52 @@
+import pandas as pd 
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split        # Para dividir los datos en conjuntos de entrenamiento y prueba
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier          # Para usar el clasificador KNN para la clasificación y regresión
+from sklearn.metrics import f1_score, confusion_matrix, ConfusionMatrixDisplay  # Para evaluar la precisión del modelo
+
+
+# Importe de datos
+path = "CSV/clientes_sin_nulos.csv"
+data = pd.read_csv(path, sep=',', encoding='latin-1')
+
+# print(data.head())      # Para corroborar que se importó correctamente
+# print(data.ComproBicicleta.value_counts()/data.shape[0])
+
+data_numeric = data.select_dtypes(include=['float64', 'int64'])
+
+list_x = ['IngresoAnual', 'TotalHijos', 'Propietario', 'CantAutomoviles', 'Edad']
+scaler = StandardScaler()
+scaler.fit(data[list_x])
+X = scaler.transform(data[list_x])
+
+y = data.ComproBicicleta
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.349, random_state=17, stratify = y)
+
+
+for k in range(1, 20, 1):
+    knn = KNeighborsClassifier(k, weights= "distance")
+    knn.fit(X_train, y_train)
+    pred_train = knn.predict(X_train)
+    pred_test = knn.predict(X_test)
+    print("_______________________________________")
+    print(f"K igual a {k}: ")
+    print(f'Accuracy de K-NN train: {knn.score(X_train, y_train):.2f}')
+    print(f"F1 de K-NN train: {f1_score(y_train,pred_train, average='macro'):.2f}")
+    print(f'Accuracy de K-NN test: {knn.score(X_test, y_test):.2f}')
+    print(f"F1 de K-NN test: {f1_score(y_test, pred_test,average='macro'):.2f}")
+
+    # Nos quedamos con k = 12
+
+
+# knn = KNeighborsClassifier(12, weights= "distance")
+# knn.fit(X_train, y_train)
+# pred = knn.predict(X_test)
+
+# cm = confusion_matrix(y_test, pred)
+# cm_display = ConfusionMatrixDisplay(cm, display_labels = knn.classes_)
+# cm_display.plot()
+
+# plt.xticks(rotation=90)
+# plt.show()
