52cefb02bfe3e0e39349cfda9c351fa622876f6a
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-05-15 17:31:33 -0300

    Fix comentarios

--- KNN.py
+++ KNN.py
@@ -10,11 +10,10 @@ from sklearn.metrics import f1_score, confusion_matrix, ConfusionMatrixDisplay  
 path = "CSV/clientes_sin_nulos.csv"
 data = pd.read_csv(path, sep=',', encoding='latin-1')
 
-# print(data.head())      # Para corroborar que se importó correctamente
-# print(data.ComproBicicleta.value_counts()/data.shape[0])
+print(data.head())      # Para corroborar que se importó correctamente
 
-data_numeric = data.select_dtypes(include=['float64', 'int64'])
 
+# Preparación del algoritmo KNN
 list_x = ['IngresoAnual', 'TotalHijos', 'Propietario', 'CantAutomoviles', 'Edad']
 scaler = StandardScaler()
 scaler.fit(data[list_x])
@@ -25,6 +24,7 @@ y = data.ComproBicicleta
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.349, random_state=17, stratify = y)
 
 
+# Cálculo de la K que vamos a utilizar
 for k in range(1, 20, 1):
     knn = KNeighborsClassifier(k, weights= "distance")
     knn.fit(X_train, y_train)
@@ -36,17 +36,17 @@ for k in range(1, 20, 1):
     print(f"F1 de K-NN train: {f1_score(y_train,pred_train, average='macro'):.2f}")
     print(f'Accuracy de K-NN test: {knn.score(X_test, y_test):.2f}')
     print(f"F1 de K-NN test: {f1_score(y_test, pred_test,average='macro'):.2f}")
+# Conclusión: nos quedamos con K = 12
 
-    # Nos quedamos con k = 12
 
+# Algoritmo KNN
+knn = KNeighborsClassifier(12, weights= "distance")
+knn.fit(X_train, y_train)
+pred = knn.predict(X_test)
 
-# knn = KNeighborsClassifier(12, weights= "distance")
-# knn.fit(X_train, y_train)
-# pred = knn.predict(X_test)
+cm = confusion_matrix(y_test, pred)
+cm_display = ConfusionMatrixDisplay(cm, display_labels = knn.classes_)
+cm_display.plot()
 
-# cm = confusion_matrix(y_test, pred)
-# cm_display = ConfusionMatrixDisplay(cm, display_labels = knn.classes_)
-# cm_display.plot()
-
-# plt.xticks(rotation=90)
-# plt.show()
+plt.xticks(rotation=90)
+plt.show()
